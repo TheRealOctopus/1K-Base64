e24a573403522ccdeb22dc2fac9c7fbc352057f3
Octopus Not Sqiud <119736925+TheRealOctopus@users.noreply.github.com>
2023-12-18 06:12:32 +0000

    new file:   .vscode/settings.json new file:   encrypt.txt modified:   main.cpp modified:   main.exe

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 using namespace base64;
 int main(int argc, char** argv) {
-    string type = argv[1];
-    if(argc <= 4, argc > 4) {
+    if(argc != 4) {
         cout << "error: ./app.exe decrypt/encrypt <step> <string>" << endl;
         cout << "Example: ./app.exe encrypt 6000 BachOctopuss" << endl;
         return 1;
     }
+    string type = argv[1];
     if(type == "decrypt") {
         string text = argv[3], temp, temp0, s = argv[2];
 
@@ -17,9 +17,10 @@ int main(int argc, char** argv) {
         int step = stoi(s);
         for(int i = 1; i<step; i++) {
             temp0 = from_base64(temp);
-            cout << endl << temp0 << endl;
+            
             temp = temp0;
         }
+        cout << endl << temp0 << endl;
     }
     if(type == "encrypt") {
         string text = argv[3], temp, temp0, s = argv[2];
@@ -28,9 +29,10 @@ int main(int argc, char** argv) {
         int step = stoi(s);
         for(int i = 1; i<step; i++) {
             temp0 = to_base64(temp);
-            cout << endl << temp0 << endl;
+            
             temp = temp0;
         }
+        cout << endl << temp0 << endl;
     }
     return 0;
 }
